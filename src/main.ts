146c6b01569537aa2cc79a69d17482c5865a0772
TDP <1240533759@qq.com>
2024-09-04 17:58:19 +0800

    初始化项目

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+// 引入创建app实例的函数
+import { createApp } from 'vue'
+// 引入app组件
+import App from './App.vue'
+// 引入路由器实例
+import router from './router'
+// 引入创建pinia的函数
+import { createPinia } from 'pinia'
+// 创建pinia实例
+const pinia = createPinia()
+// 创建app应用实例
+const app = createApp(App)
+// 注册路由插件
+app.use(router)
+// 注册pinia插件
+app.use(pinia)
+// 将app应用实例挂载到挂载点上
+app.mount('#app')
---
+++ src/views/index.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>哈哈哈</div>
+</template>
+
+<style scoped></style>
